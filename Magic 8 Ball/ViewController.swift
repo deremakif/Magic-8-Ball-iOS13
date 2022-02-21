b4d385f95a6779af98d740f06da5812a2f46214e
akif <mehmetakif.dere@halkbank.com.tr>
2022-02-21 10:07:49 +0300

    design linked up with the code

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,25 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
+//    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+//    override class func initialize() {
+//
+//    }
+    
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        print("ğğğğğ");
+        
+    }
+    
+    
 
 }
 
